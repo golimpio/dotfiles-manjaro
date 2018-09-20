c4c1db9373e48b0ebe0d79d31157604e0ff9ee5c
Gilberto Olimpio <golimpio@gmail.com>
2018-09-21 09:27:25 +1000

    Added polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch "main" bar on all monitors
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar --reload main &
+done
+
+echo "Bars launched..."
+
