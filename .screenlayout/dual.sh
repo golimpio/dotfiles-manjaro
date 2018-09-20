9e30b61cbddd436855459d01c3037c2a8bede112
Gilberto Olimpio <golimpio@gmail.com>
2018-09-20 15:20:04 +1000

    Added screenlayout and Xresources

---
+++ .screenlayout/dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP1 --mode 3840x2160 --pos 3840x0 --scale 1.35x1.35 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
