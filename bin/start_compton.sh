dc9d4ceea727206bb0349d07ceaf57c185353418
Gilberto Olimpio <golimpio@gmail.com>
2018-09-20 22:17:54 +1000

    Added xmodmap files

--- bin/start_compton.sh
+++ bin/start_compton.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-compton -b --blur-method kawase --backend glx
+compton -b --blur-method kawase --backend glx --glx-no-stencil --unredir-if-possible --glx-no-rebind-pixmap --xrender-sync-fence
 
