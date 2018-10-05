8441ae3e9e9ee22d9434b9ad559a9ee33e168f7c
Gilberto Olimpio <golimpio@gmail.com>
2018-10-05 18:05:36 +1000

    Update settings + power menu

---
+++ bin/power.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# Simple script to handle a DIY shutdown menu. When run you should see a bunch of options (shutdown, reboot etc.)
+#
+# Requirements:
+# - rofi
+# - systemd, but you can replace the commands for OpenRC or anything else
+#
+# Instructions:
+# - Save this file as power.sh or anything
+# - Give it exec priviledge, or chmod +x /path/to/power.sh
+# - Run it
+#
+# Source:
+# https://gist.github.com/adnan360/f86012baeb4c9ca4f1af033550b03033
+#
+
+chosen=$(echo -e "\
+ Cancel\n\
+ Display Off\n\
+ Lock\n\
+ Logout\n\
+ Shutdown\n\
+ Reboot\n\
+ Suspend\n\
+ Hibernate" | rofi -dmenu -i -config ~/.config/rofi/power_menu_config)
+# Info about some states are available here:
+# https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
+
+if [[ $chosen = " Lock" ]]; then
+	jwm -exit
+if [[ $chosen = " Logout" ]]; then
+	jwm -exit
+elif [[ $chosen = " Shutdown" ]]; then
+	systemctl poweroff
+elif [[ $chosen = " Reboot" ]]; then
+	systemctl reboot
+elif [[ $chosen = " Suspend" ]]; then
+	systemctl suspend
+elif [[ $chosen = " Hibernate" ]]; then
+	systemctl hibernate
+fi
